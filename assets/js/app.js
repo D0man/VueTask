f69d5f08b71318777af308af9071e17ae20dfdca
d0man <jakub.tomasz.domanski@gmail.com>
2018-06-05 22:15:42 +0200

    small adjustment with spaces in js and added pointer to buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
  Vue.component('todo', {
     template: `<div class="container">
-            <div class="container__buttons">
-            <button @click="addTask" class="button button--add">
-              Add
-            </button>
-            <button v-if="taskList.length" @click="removeTask" class="button button--remove">
-              Remove
-            </button>
-            <button v-if="taskList.length"  @click="clearTasks" class=" button button--clear">
-              Clear
-            </button>
-          </div>
-          <transition-group name="task" tag="ul" class="tasks">
-            <li v-for="(task, index) in taskList" :key="index" class="task">
-              {{ task }} {{ index }}
-            </li>
-          </transition-group>
-        </div>`,
+                    <div class="container__buttons">
+                        <button @click="addTask" class="button button--add">
+                        Add
+                        </button>
+                        <button v-if="taskList.length" @click="removeTask" class="button button--remove">
+                        Remove
+                        </button>
+                        <button v-if="taskList.length"  @click="clearTasks" class=" button button--clear">
+                        Clear
+                        </button>
+                    </div>
+                    <transition-group name="task" tag="ul" class="tasks">
+                        <li v-for="(task, index) in taskList" :key="index" class="task">
+                        {{ task }} {{ index }}
+                        </li>
+                    </transition-group>
+                </div>`,
         data: function() {
           return {
             taskList: [],
@@ -24,19 +24,19 @@
           }
         },
         methods: {
-        addTask(){
-         this.taskList.push(this.taskName);
-        },
-        removeTask(){
-          this.taskList.pop();
-        },
-        clearTasks(){
-          this.taskList = [];
-        },
-        
-      }
+            addTask(){
+            this.taskList.push(this.taskName);
+            },
+            removeTask(){
+            this.taskList.pop();
+            },
+            clearTasks(){
+            this.taskList = [];
+            },
+        }
     });
-    var app = new Vue({
+
+    const app = new Vue({
       el: '#app',
       data: {
         title: 'Simple Vue to do list without to do'
