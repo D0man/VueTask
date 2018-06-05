2e14c161308f61140ee5b54d2471036ce2cd6a81
d0man <jakub.tomasz.domanski@gmail.com>
2018-06-05 22:01:43 +0200

    update file structure and making js component style

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
  I was ask to made simple to do up in Vue. The idea is to make 3 buttons that adds task, removing last task and clearing all task. 
 ## Live version
 [VueTask](https://d0man.github.io/VueTask/)
-##File mangament
+## File mangament
 I didn't bother creating additinal folders for my app i just created index.html via vue-cli simple and add css file.
-##main.css
-I didn't sass or autoprefixer cuz app is very small, it should work on modern browsers but in older it can have some problems with flex. I didn't use any normalizer or more specyfic reset(usaly i do);
-##index.html
-I didn't bother with creating js file cuz this way is "more vue", usually i work with webpack and use templates in vue file to create components. Also i didnt add meta tags etc.
-##javascript
-JavaScript is writen in es6 so it could not work in older browsers, again usually webpack and babel takes care for me in this
+## css
+I usually write my apps with sass and post-css to add prefixes, but this app is simple so decided to not doing this. Still i used flex without prefixes so it can be buged on older browsers
+
+## javascript
+JavaScript is writen in es6 so it could not work in older browsers
---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+ Vue.component('todo', {
+    template: `<div class="container">
+            <div class="container__buttons">
+            <button @click="addTask" class="button button--add">
+              Add
+            </button>
+            <button v-if="taskList.length" @click="removeTask" class="button button--remove">
+              Remove
+            </button>
+            <button v-if="taskList.length"  @click="clearTasks" class=" button button--clear">
+              Clear
+            </button>
+          </div>
+          <transition-group name="task" tag="ul" class="tasks">
+            <li v-for="(task, index) in taskList" :key="index" class="task">
+              {{ task }} {{ index }}
+            </li>
+          </transition-group>
+        </div>`,
+        data: function() {
+          return {
+            taskList: [],
+            taskName: 'Task number'
+          }
+        },
+        methods: {
+        addTask(){
+         this.taskList.push(this.taskName);
+        },
+        removeTask(){
+          this.taskList.pop();
+        },
+        clearTasks(){
+          this.taskList = [];
+        },
+        
+      }
+    });
+    var app = new Vue({
+      el: '#app',
+      data: {
+        title: 'Simple Vue to do list without to do'
+      },
+    })
